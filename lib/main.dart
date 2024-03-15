8bf68eac7f37936d049c809e899d582ec3eaacf1
Anar <anar.abdullazada@ufaz.az>
2024-03-15 17:41:20 +0400

    ScrollHomePageListView is now abstract

--- lib/main.dart
+++ lib/main.dart
@@ -32,28 +32,6 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => MyHomePageState();
 }
 
-class ScrollHomePageListView {
-  void scroll(MyHomePageState hPState) {
-    double lastMessageBoxHeight = 0;
-    if (hPState.messageBoxList.length > 1) {
-      if (hPState.gKey.currentContext?.size?.height is double) {
-        lastMessageBoxHeight =
-            hPState.gKey.currentContext?.size?.height as double;
-      } else {
-        throw ErrorDescription("Scroll's max extent is not correct");
-      }
-    }
-    if (hPState.listViewScrollController.position.maxScrollExtent > 0) {
-      hPState.listViewScrollController.animateTo(
-        hPState.listViewScrollController.position.maxScrollExtent +
-            lastMessageBoxHeight,
-        duration: const Duration(milliseconds: 300),
-        curve: Curves.easeOut,
-      );
-    }
-  }
-}
-
 void addAMessageBox2ViewList(MyHomePageState hPState, value) {
   if (hPState.messageBoxList.isNotEmpty) {
     Text oldLastMesaageBox = hPState.messageBoxList
@@ -63,33 +41,8 @@ void addAMessageBox2ViewList(MyHomePageState hPState, value) {
   hPState.messageBoxList.add(Text(value, key: hPState.gKey));
 }
 
-TextField textField(MyHomePageState hPState) {
-  return TextField(
-    controller: hPState.sendTextController,
-    focusNode: hPState.sendTextFocusNode,
-    onSubmitted: (value) {
-      //TODO: understand how sleeps stops both setState and print.
-      hPState.setState(() {
-        addAMessageBox2ViewList(hPState, value);
-        hPState.sendTextController.clear();
-        hPState.sendTextFocusNode.requestFocus();
-        ScrollHomePageListView().scroll(hPState);
-      });
-      sleep(Durations.extralong4);
-      print(hPState.listViewScrollController.position.maxScrollExtent);
-    },
-    decoration: const InputDecoration(
-      hintTextDirection: TextDirection.ltr,
-      labelText: "Send message",
-      hintText: "type...",
-      fillColor: Color.fromARGB(255, 255, 255, 255),
-    ),
-  );
-}
-
 class MyHomePageState extends State<MyHomePage> {
   List<Widget> messageBoxList = [];
-
   final gKey = GlobalKey();
 
   TextEditingController sendTextController = TextEditingController();
@@ -135,5 +88,51 @@ class MyHomePageState extends State<MyHomePage> {
         ),
       ),
     );
+  }
+}
+
+TextField textField(MyHomePageState hPState) {
+  return TextField(
+    controller: hPState.sendTextController,
+    focusNode: hPState.sendTextFocusNode,
+    onSubmitted: (value) {
+      //TODO: understand how sleeps stops both setState and print.
+      hPState.setState(() {
+        addAMessageBox2ViewList(hPState, value);
+        hPState.sendTextController.clear();
+        hPState.sendTextFocusNode.requestFocus();
+        ScrollHomePageListView.scroll(hPState);
+      });
+      sleep(Durations.extralong4);
+      print(hPState.listViewScrollController.position.maxScrollExtent);
+    },
+    decoration: const InputDecoration(
+      hintTextDirection: TextDirection.ltr,
+      labelText: "Send message",
+      hintText: "type...",
+      fillColor: Color.fromARGB(255, 255, 255, 255),
+    ),
+  );
+}
+
+abstract class ScrollHomePageListView {
+  static void scroll(MyHomePageState hPState) {
+    double lastMessageBoxHeight = 0;
+    if (hPState.messageBoxList.length > 1) {
+      if (hPState.gKey.currentContext?.size?.height is double) {
+        lastMessageBoxHeight =
+            hPState.gKey.currentContext?.size?.height as double;
+      } else {
+        throw ErrorDescription("Scroll's max extent is not correct");
+      }
+    }
+    if (hPState.listViewScrollController.position.maxScrollExtent > 0) {
+      hPState.listViewScrollController.animateTo(
+        hPState.listViewScrollController.position.maxScrollExtent +
+            lastMessageBoxHeight,
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeOut,
+      );
+    }
   }
 }
